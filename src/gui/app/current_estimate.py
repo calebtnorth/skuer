69f3caa42c3b7f15a569bf9e9769658fc4e3bbe2
calebtnorth <contact@fivesixfive.dev>
2025-08-24 21:43:21 -0500

    Building out some inventory GUI functionality

--- src/gui/app/current_estimate.py
+++ src/gui/app/current_estimate.py
@@ -4,7 +4,8 @@ from typing import Self
 
 from PySide6.QtWidgets import (
     QWidget, QGroupBox,
-    QHBoxLayout, QSizePolicy
+    QGridLayout, QVBoxLayout, QSizePolicy,
+    QLabel, QListWidget
 )
 from PySide6.QtCore import (
     QRect
@@ -19,6 +20,40 @@ class CurrentEstimateCardWidget(QGroupBox):
     def __init__(self: Self, *arg) -> None:
         super().__init__(*arg)
 
+        # List Widgets
+        self.estimated_products_list_widget     = QListWidget()
+        self.estimated_expenses_list_widget     = QListWidget()
+        self.estimated_fees_list_widget         = QListWidget()
+
+        # Labels
+        self.estimate_card_numbers_layout       = QGridLayout()
+
+        self.estimated_cost_label       = QLabel("$0.00")
+        self.estimated_income_label     = QLabel("$0.00")
+        self.estimated_expense_label    = QLabel("$0.00")
+        
+        self.actual_cost_label          = QLabel("$0.00")
+        self.actual_income_label        = QLabel("$0.00")
+        self.actual_expense_label       = QLabel("$0.00")
+
+        self.estimate_card_numbers_layout.addWidget(self.estimated_cost_label, 0, 0, 1, 1)
+        self.estimate_card_numbers_layout.addWidget(self.estimated_income_label, 2, 0, 1, 1)
+        self.estimate_card_numbers_layout.addWidget(self.estimated_expense_label, 2, 1, 1, 1)
+
+        self.estimate_card_numbers_layout.addWidget(self.actual_cost_label, 1, 0, 1, 1)
+        self.estimate_card_numbers_layout.addWidget(self.actual_income_label, 3, 0, 1, 1)
+        self.estimate_card_numbers_layout.addWidget(self.actual_expense_label, 3, 1, 1, 1)
+
+        self.estimate_card_numbers_layout.setColumnStretch(0, 0)
+        self.estimate_card_numbers_layout.setColumnStretch(1, 0)
+        self.estimate_card_numbers_layout.setColumnStretch(2, 1)
+
+        # Main Layout
+        self.estimate_card_widget_layout = QVBoxLayout(self)
+        self.estimate_card_widget_layout.addLayout(self.estimate_card_numbers_layout)
+        self.estimate_card_widget_layout.addWidget(self.estimated_products_list_widget)
+        self.estimate_card_widget_layout.addWidget(self.estimated_expenses_list_widget)
+        self.estimate_card_widget_layout.addWidget(self.estimated_fees_list_widget)
 
 class CurrentEstimateToolbarWidget(QWidget):
     """
--- src/gui/app/inventory.py
+++ src/gui/app/inventory.py
@@ -5,14 +5,50 @@ from typing import Self
 from PySide6.QtWidgets import (
     QGroupBox,
     QGridLayout, QHBoxLayout,
-    QPushButton, QListWidget
+    QComboBox, QLabel, QListWidget, QListWidgetItem, QPushButton, QWidget
 )
+
+
+class ProductListWidgetItemWidget(QWidget):
+    """
+    
+    """
+
+    def __init__(self: Self, *arg) -> None:
+        super().__init__(*arg)
+
+        self.product_list_widget_item_layout = QHBoxLayout(self)
+        
+        self.product_list_widget_item_combobox          = QComboBox()
+        self.product_list_widget_item_sold_label        = QLabel("0")
+        self.product_list_widget_item_in_stock_label    = QLabel("0")
+        self.product_list_widget_item_total_label       = QLabel("0")
+
+        self.product_list_widget_item_layout.addWidget(self.product_list_widget_item_combobox)
+        self.product_list_widget_item_layout.addStretch(1)
+        self.product_list_widget_item_layout.addWidget(self.product_list_widget_item_sold_label)
+        self.product_list_widget_item_layout.addWidget(self.product_list_widget_item_in_stock_label)
+        self.product_list_widget_item_layout.addWidget(self.product_list_widget_item_total_label)
 
 
 class InventoryWidget(QGroupBox):
     """
     Displays the current inventory, regardless of estimate card
     """
+
+    def add_product_list_widget_item(self: Self, item_widget: ProductListWidgetItemWidget) -> QListWidgetItem:
+        """
+        Add a new QListWidgetItem with an InventoryListWidgetItemWidget assigned to it
+        """
+
+        new_qlist_widget_item = QListWidgetItem()
+
+        self.product_list_widget.addItem(new_qlist_widget_item)
+        self.product_list_widget.setItemWidget(new_qlist_widget_item, item_widget)
+        
+        new_qlist_widget_item.setSizeHint(item_widget.sizeHint())
+        return new_qlist_widget_item
+    
 
     def __init__(self: Self, *arg) -> None:
         super().__init__(*arg)
@@ -33,8 +69,10 @@ class InventoryWidget(QGroupBox):
         self.product_list_push_button_layout.addStretch(1)
         self.product_list_push_button_layout.addWidget(self.product_list_estimate_push_button)
 
-        # Inventory block layout
+        # Main layout
         self.inventory_widget_layout = QGridLayout(self)
         self.inventory_widget_layout.addWidget(self.product_list_widget, 0, 0, 1, 1)
         self.inventory_widget_layout.addWidget(self.parts_list_widget, 0, 1, 1, 1)
         self.inventory_widget_layout.addLayout(self.product_list_push_button_layout, 1, 0, 1, 1)
+
+        self.add_product_list_widget_item(ProductListWidgetItemWidget())
